4a5f76b97a82237e5b8ccccb984889dbd91f5603
USHASRI1112 <96167956+USHASRI1112@users.noreply.github.com>
2026-03-17 17:21:15 +0530

    Update axios.js

--- maintenance-system-frontend/src/api/axios.js
+++ maintenance-system-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://maintenance-system-1-hwot.onrender.com/",
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://maintenance-system-1-hwot.onrender.com/',
 })
 
 // Attach token to every request automatically
